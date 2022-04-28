a9b9501e14ef015e9be404d61fc4de99e06e92fc
pH2O4 <ph2oalencar@gmail.com>
2022-04-28 17:04:17 -0300

    ---

---
+++ allapi/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import Barchart from './components/APIBarchart'
+
+function App() {
+  return (
+    <div className="App">
+      <Barchart />
+    </div>
+  );
+}
+
+export default App;
---
+++ allapi/src/components/APIBarchart.jsx
@@ -0,0 +1,25 @@
+import {Component, React} from "react";
+
+
+class Barchart extends Component {
+
+  state = {
+    tables: [],
+  }
+
+  async  componentDidMount() {
+
+  }
+    render(){
+      window.onload(
+        
+      )
+        return(
+            <div>
+              <h1>1 - </h1>
+            </div>
+        )
+    }
+}
+
+export default Barchart
