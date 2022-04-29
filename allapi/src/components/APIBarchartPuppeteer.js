0263765ac85b7fa561992b180b3cddb3f95599ab
pH2O4 <ph2oalencar@gmail.com>
2022-04-29 16:18:44 -0300

    -

--- allapi/src/components/APIBarchartPuppeteer.js
+++ allapi/src/components/APIBarchartPuppeteer.js
@@ -1,37 +1,39 @@
 
+const { type } = require('express/lib/response');
 const Puppeteer = require('puppeteer');
 
 const myDatasTrated = () => {
-    
+
     (async () => {
         const browser = await Puppeteer.launch({ headless: false });
         const page = await browser.newPage();
         await page.goto('https://www.barchart.com/futures/quotes/CTK20/futures-prices');
 
-        const myDatasTrated =  page.evaluate( async () => {
-            const THEADBarchart = await document.querySelectorAll("th")
-            let THEADBarchartArray = []
-            for (let indexTHEAD = 0; indexTHEAD < THEADBarchart.length; indexTHEAD++) {
-                const elementsTHEADBarchart = THEADBarchart[indexTHEAD].textContent
-                THEADBarchartArray.push(elementsTHEADBarchart)
-            }
+        const myDatasTrated = page.evaluate(async () => {
+            /*  const THEADBarchart = await document.querySelectorAll("th")
+              let THEADBarchartArray = []
+              for (let indexTHEAD = 0; indexTHEAD < THEADBarchart.length; indexTHEAD++) {
+                  const elementsTHEADBarchart = THEADBarchart[indexTHEAD].textContent
+                  THEADBarchartArray.push(elementsTHEADBarchart)
+              }*/
 
             const TBODYBarchart = await document.querySelectorAll("tr")
             let TBODYBarchartArray = []
             for (let indexBODY = 0; indexBODY < TBODYBarchart.length; indexBODY++) {
                 const elementsBODYBarchart = TBODYBarchart[indexBODY].textContent
-                console.log(elementsBODYBarchart, indexBODY)
-                TBODYBarchartArray.push(elementsBODYBarchart)
-                const elementsBODYBarchartEmpty = elementsBODYBarchart.replace(/\n/g, "")
-                console.log(TBODYBarchartArray)
+                const elementsBODYBarchartClean = elementsBODYBarchart.replace(/(\r\n|\n|\r)/gm, " ")
+               const elementsBODYBarchartSpliting = elementsBODYBarchartClean.replace(/\s{2,}/g, ' ')
+               const elementsBODYBarchartSplitingMore = elementsBODYBarchartSpliting.split(" ")
+                TBODYBarchartArray.push(elementsBODYBarchartSpliting)
+                console.log(TBODYBarchartArray[0])
+
             }
-            return THEADBarchart, TBODYBarchart
+            return TBODYBarchart
         })
 
         return myDatasTrated
 
-
-       //  await browser.close()
+        //  await browser.close()
     })()
 }
 
