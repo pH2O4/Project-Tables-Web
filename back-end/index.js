ad4258ffc7fde33cba58f280e1d81805d44dace7
pH2O4 <ph2oalencar@gmail.com>
2022-05-05 22:10:19 -0300

    ---

--- back-end/index.js
+++ back-end/index.js
@@ -46,23 +46,23 @@ app.get("/GettingDatasB3", async (req, res) => {
 
     const browser = await Puppeteer.launch({ headless: false });
     const page = await browser.newPage();
-    await page.goto('https://www.b3.com.br/pt_br/market-data-e-indices/servicos-de-dados/market-data/historico/derivativos/ajustes-do-pregao/', { waitUntil: 'domcontentloaded' });
+    await page.goto('https://www2.bmf.com.br/pages/portal/bmfbovespa/lumis/lum-ajustes-do-pregao-ptBR.asp', { waitUntil: 'domcontentloaded' });
 
 
-    await page.mouse.click(381, 749, { button: 'right' })
-    await page.mouse.click(437, 732, { button: 'left' })
-    await page.mouse.click(381, 749, { button: 'right' })
-    await page.mouse.click(437, 732, { button: 'left' })
+  //  await page.mouse.click(381, 749, { button: 'right' })
+  //  await page.mouse.click(437, 732, { button: 'left' })
+  //  await page.mouse.click(381, 749, { button: 'right' })
+//    await page.mouse.click(437, 732, { button: 'left' })
  // const screenshot =  await page.screenshot({'clip':{'x': 15, 'y': 400, 'height': 506, 'width': 1562}})
   //  await page.mouse.click( 284, 920, {button: 'right'})
    // await page.mouse.click( 337, 904, {button: 'left'})
     const GetingDatas = await page.evaluate(async () => {
 
-        function storePosition(e) {
+    /*    function storePosition(e) {
             lastClickPosition = { x: e.pageX, y: e.pageY };
             console.log(lastClickPosition);
-        }
-        document.addEventListener('mouseover', storePosition, true);
+        } 
+        document.addEventListener('mouseover', storePosition, true);*/
         const TBODYB3 = await document.querySelectorAll('tr')
         let TBODYB3Array = []
         console.log(TBODYB3)
