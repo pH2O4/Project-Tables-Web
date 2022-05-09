19ca3113516b7769f1e0b08ecf4db24f0d67f871
pH2O4 <ph2oalencar@gmail.com>
2022-05-09 16:00:36 -0300

    ---

--- allapi/src/components/APIALLTABLES.js
+++ allapi/src/components/APIALLTABLES.js
@@ -3,7 +3,9 @@ import { Table } from 'react-bootstrap'
 import './TABLES.css'
 import Axios from 'axios'
 
-const Main = () => {
+class Main extends Component {
+
+  render() {
     const firtTableBarchart = async () => {
       const response = await Axios.get('http://localhost:8080/GettingDatasBarchart')
       const ArrayTableBarchart = response.data
@@ -885,210 +887,211 @@ const Main = () => {
       await fourthTable()
     }
     window.onload = CallFunctionsForEachOne()
- 
-
 
 
 
     return (
       <div className="App">
-      <div className="TABLESAPIIComponent">
-        <div className="BarchartTable">
-          <h1 id="FirtTableTitle">Barchart Table</h1>
-          <Table id="DinamicTable" striped bordered hover variant="dark">
-            <tbody >
-              <tr id="0"></tr>
-              <tr id="1">
-                <td ></td>
-                <td id="1TD"></td>
-              </tr>
-              <tr id="2">
-                <td ></td>
-                <td id="2TD"></td>
-              </tr>
-              <tr id="3">
-                <td ></td>
-                <td id="3TD"></td>
-              </tr>
-              <tr id="4">
-                <td ></td>
-                <td id="4TD"></td>
-              </tr>
-              <tr id="5">
-                <td ></td>
-                <td id="5TD"></td>
-              </tr>
-              <tr id="6">
-                <td ></td>
-                <td id="6TD"></td>
-              </tr>
-              <tr id="7">
-                <td ></td>
-                <td id="7TD"></td>
-              </tr>
-              <tr id="8">
-                <td ></td>
-                <td id="8TD"></td>
-              </tr>
-              <tr id="9">
-                <td ></td>
-                <td id="9TD"></td>
-              </tr>
-              <tr id="10">
-                <td ></td>
-                <td id="10TD"></td>
-              </tr>
-              <tr id="11">
-                <td ></td>
-                <td id="11TD"></td>
-              </tr>
-              <tr id="12">
-                <td ></td>
-                <td id="12TD"></td>
-              </tr>
-              <tr id="13">
-                <td ></td>
-                <td id="13TD"></td>
-              </tr>
-              <tr id="14">
-                <td ></td>
-                <td id="14TD"></td>
-              </tr>
-              <tr id="15">
-                <td ></td>
-                <td id="15TD"></td>
-              </tr>
-            </tbody>
-          </Table>
-        </div>
+        <div className="TABLESAPIIComponent">
+          <div className="BarchartTable">
+            <h1 id="FirtTableTitle">Barchart Table</h1>
+            <Table id="DinamicTable" striped bordered hover variant="dark">
+              <tbody >
+                <tr id="0"></tr>
+                <tr id="1">
+                  <td ></td>
+                  <td id="1TD"></td>
+                </tr>
+                <tr id="2">
+                  <td ></td>
+                  <td id="2TD"></td>
+                </tr>
+                <tr id="3">
+                  <td ></td>
+                  <td id="3TD"></td>
+                </tr>
+                <tr id="4">
+                  <td ></td>
+                  <td id="4TD"></td>
+                </tr>
+                <tr id="5">
+                  <td ></td>
+                  <td id="5TD"></td>
+                </tr>
+                <tr id="6">
+                  <td ></td>
+                  <td id="6TD"></td>
+                </tr>
+                <tr id="7">
+                  <td ></td>
+                  <td id="7TD"></td>
+                </tr>
+                <tr id="8">
+                  <td ></td>
+                  <td id="8TD"></td>
+                </tr>
+                <tr id="9">
+                  <td ></td>
+                  <td id="9TD"></td>
+                </tr>
+                <tr id="10">
+                  <td ></td>
+                  <td id="10TD"></td>
+                </tr>
+                <tr id="11">
+                  <td ></td>
+                  <td id="11TD"></td>
+                </tr>
+                <tr id="12">
+                  <td ></td>
+                  <td id="12TD"></td>
+                </tr>
+                <tr id="13">
+                  <td ></td>
+                  <td id="13TD"></td>
+                </tr>
+                <tr id="14">
+                  <td ></td>
+                  <td id="14TD"></td>
+                </tr>
+                <tr id="15">
+                  <td ></td>
+                  <td id="15TD"></td>
+                </tr>
+              </tbody>
+            </Table>
+          </div>
 
-        <div className="CmegroupTable">
-          <h1>Cmegroup Table</h1>
-          <Table id="SecondTableCmegroup" striped bordered hover variant="light">
-            <tbody>
-              <tr>
-                <td ><b>Month</b></td>
-                <td><b>Options</b></td>
-                <td><b>Chart</b></td>
-                <td><b>Last</b></td>
-                <td><b>Change</b></td>
-                <td><b>Prior Settle</b></td>
-                <td><b>Open</b></td>
-                <td><b>High</b></td>
-                <td><b>Low</b></td>
-                <td><b>Volume</b></td>
-                <td><b>Updated</b></td>
-              </tr>
-              <tr id="0C"></tr>
-              <tr id="2C"></tr>
-              <tr id="3C"></tr>
-              <tr id="4C"></tr>
-              <tr id="5C"></tr>
-              <tr id="6C"></tr>
-              <tr id="7C"></tr>
-              <tr id="8C"></tr>
-              <tr id="9C"></tr>
-              <tr id="10C"></tr>
-              <tr id="11C"></tr>
-              <tr id="12C"></tr>
-              <tr id="13C"></tr>
-              <tr id="14C"></tr>
-              <tr id="15C"></tr>
-              <tr id="16C"></tr>
-              <tr id="17C"></tr>
-              <tr id="18C"></tr>
-            </tbody>
-          </Table>
-        </div>
+          <div className="CmegroupTable">
+            <h1>Cmegroup Table</h1>
+            <Table id="SecondTableCmegroup" striped bordered hover variant="light">
+              <tbody>
+                <tr>
+                  <td ><b>Month</b></td>
+                  <td><b>Options</b></td>
+                  <td><b>Chart</b></td>
+                  <td><b>Last</b></td>
+                  <td><b>Change</b></td>
+                  <td><b>Prior Settle</b></td>
+                  <td><b>Open</b></td>
+                  <td><b>High</b></td>
+                  <td><b>Low</b></td>
+                  <td><b>Volume</b></td>
+                  <td><b>Updated</b></td>
+                </tr>
+                <tr id="0C"></tr>
+                <tr id="2C"></tr>
+                <tr id="3C"></tr>
+                <tr id="4C"></tr>
+                <tr id="5C"></tr>
+                <tr id="6C"></tr>
+                <tr id="7C"></tr>
+                <tr id="8C"></tr>
+                <tr id="9C"></tr>
+                <tr id="10C"></tr>
+                <tr id="11C"></tr>
+                <tr id="12C"></tr>
+                <tr id="13C"></tr>
+                <tr id="14C"></tr>
+                <tr id="15C"></tr>
+                <tr id="16C"></tr>
+                <tr id="17C"></tr>
+                <tr id="18C"></tr>
+              </tbody>
+            </Table>
+          </div>
 
-        <div className="TableB3">
-          <h1>B3 Table</h1>
-          <Table id="B3TABLEX" striped bordered hover variant="dark">
-            <tbody id="BodyTableB3">
-              <tr><td><b>Mercadoria</b></td>  <td><b>Vencimento</b></td>
-                <td><b>Preço de Ajuste Anterior</b></td>
-                <td><b>Preço de Ajuste Atual	</b></td>
-                <td><b>Variação</b></td>
-                <td><b>Valor do Ajuste Por Contrato (R$)</b></td></tr>
+          <div className="TableB3">
+            <h1>B3 Table</h1>
+            <Table id="B3TABLEX" striped bordered hover variant="dark">
+              <tbody id="BodyTableB3">
+                <tr><td><b>Mercadoria</b></td>  <td><b>Vencimento</b></td>
+                  <td><b>Preço de Ajuste Anterior</b></td>
+                  <td><b>Preço de Ajuste Atual	</b></td>
+                  <td><b>Variação</b></td>
+                  <td><b>Valor do Ajuste Por Contrato (R$)</b></td></tr>
 
-              <tr id="B3TR1"></tr>
-              <tr id="B3TR2"></tr>
-              <tr id="B3TR3"></tr>
-              <tr id="B3TR4"></tr>
-              <tr id="B3TR5"></tr>
-              <tr id="B3TR6"></tr>
-              <tr id="B3TR7"></tr>
-              <tr id="B3TR8"></tr>
-              <tr id="B3TR9"></tr>
-              <tr id="B3TR10"></tr>
-              <tr id="B3TR11"></tr>
-              <tr id="B3TR12"></tr>
-              <tr id="B3TR13"></tr>
-              <tr id="B3TR14"></tr>
-              <tr id="B3TR15"></tr>
-              <tr id="B3TR16"></tr>
-              <tr id="B3TR17"></tr>
-              <tr id="B3TR18"></tr>
-              <tr id="B3TR19"></tr>
-              <tr id="B3TR20"></tr>
-              <tr id="B3TR21"></tr>
-              <tr id="B3TR22"></tr>
-              <tr id="B3TR23"></tr>
-              <tr id="B3TR24"></tr>
-              <tr id="B3TR25"></tr>
-              <tr id="B3TR26"></tr>
-              <tr id="B3TR27"></tr>
-              <tr id="B3TR28"></tr>
-              <tr id="B3TR29"></tr>
-              <tr id="B3TR30"></tr>
-              <tr id="B3TR31"></tr>
-              <tr id="B3TR32"></tr>
-              <tr id="B3TR33"></tr>
-              <tr id="B3TR34"></tr>
-            </tbody>
-          </Table>
-        </div>
+                <tr id="B3TR1"></tr>
+                <tr id="B3TR2"></tr>
+                <tr id="B3TR3"></tr>
+                <tr id="B3TR4"></tr>
+                <tr id="B3TR5"></tr>
+                <tr id="B3TR6"></tr>
+                <tr id="B3TR7"></tr>
+                <tr id="B3TR8"></tr>
+                <tr id="B3TR9"></tr>
+                <tr id="B3TR10"></tr>
+                <tr id="B3TR11"></tr>
+                <tr id="B3TR12"></tr>
+                <tr id="B3TR13"></tr>
+                <tr id="B3TR14"></tr>
+                <tr id="B3TR15"></tr>
+                <tr id="B3TR16"></tr>
+                <tr id="B3TR17"></tr>
+                <tr id="B3TR18"></tr>
+                <tr id="B3TR19"></tr>
+                <tr id="B3TR20"></tr>
+                <tr id="B3TR21"></tr>
+                <tr id="B3TR22"></tr>
+                <tr id="B3TR23"></tr>
+                <tr id="B3TR24"></tr>
+                <tr id="B3TR25"></tr>
+                <tr id="B3TR26"></tr>
+                <tr id="B3TR27"></tr>
+                <tr id="B3TR28"></tr>
+                <tr id="B3TR29"></tr>
+                <tr id="B3TR30"></tr>
+                <tr id="B3TR31"></tr>
+                <tr id="B3TR32"></tr>
+                <tr id="B3TR33"></tr>
+                <tr id="B3TR34"></tr>
+              </tbody>
+            </Table>
+          </div>
 
-        <div className="bcbgovTablefourth">
-          <h1 id="fourthTableTitle">USD Cotação GOV</h1>
-          <Table id="Fourthtable" striped bordered hover variant="light">
-            <tbody >
-              <tr>
-                <td>Data</td>
-                <td>tipo</td>
-                <td colSpan={2}>Cotações em Real1/</td>
-              </tr>
-              <tr>
-                <td></td>
-                <td></td>
-                <td>Compra	</td>
-                <td>Venda</td>
-              </tr>
-              <tr id="1GOVROW"></tr>
-              <tr id="2GOVROW"></tr>
-              <tr id="3GOVROW"></tr>
-              <tr id="4GOVROW"></tr>
-              <tr id="5GOVROW"></tr>
-              <tr id="6GOVROW"></tr>
-              <tr id="7GOVROW"></tr>
-              <tr id="8GOVROW"></tr>
-              <tr id="9GOVROW"></tr>
-              <tr id="10GOVROW"></tr>
-              <tr id="11GOVROW"></tr>
-              <tr id="12GOVROW"></tr>
-              <tr id="14GOVROW"></tr>
-              <tr id="15GOVROW"></tr>
-              <tr id="16GOVROW"></tr>
-              <tr id="17GOVROW"></tr>
-              <tr id="18GOVROW"></tr>
-              <tr id="19GOVROW"></tr>
-            </tbody>
-          </Table>
+          <div className="bcbgovTablefourth">
+            <h1 id="fourthTableTitle">USD Cotação GOV</h1>
+            <Table id="Fourthtable" striped bordered hover variant="light">
+              <tbody >
+                <tr>
+                  <td>Data</td>
+                  <td>tipo</td>
+                  <td colSpan={2}>Cotações em Real1/</td>
+                </tr>
+                <tr>
+                  <td></td>
+                  <td></td>
+                  <td>Compra	</td>
+                  <td>Venda</td>
+                </tr>
+                <tr id="1GOVROW"></tr>
+                <tr id="2GOVROW"></tr>
+                <tr id="3GOVROW"></tr>
+                <tr id="4GOVROW"></tr>
+                <tr id="5GOVROW"></tr>
+                <tr id="6GOVROW"></tr>
+                <tr id="7GOVROW"></tr>
+                <tr id="8GOVROW"></tr>
+                <tr id="9GOVROW"></tr>
+                <tr id="10GOVROW"></tr>
+                <tr id="11GOVROW"></tr>
+                <tr id="12GOVROW"></tr>
+                <tr id="14GOVROW"></tr>
+                <tr id="15GOVROW"></tr>
+                <tr id="16GOVROW"></tr>
+                <tr id="17GOVROW"></tr>
+                <tr id="18GOVROW"></tr>
+                <tr id="19GOVROW"></tr>
+              </tbody>
+            </Table>
+          </div>
         </div>
-      </div>
       </div>
     )
-  
+  }
+
+
+
 }
 
 export default Main
